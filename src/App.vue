c14a2cae961a93460f3cbe4beba83b0bfa551108
Mrr-cxh <1361091680@qq.com>
2021-05-11 19:59:22 +0800

    新增了aaa

--- src/App.vue
+++ src/App.vue
@@ -2,16 +2,19 @@
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png" />
     <HelloWorld msg="Welcome to Your Vue.js App" />
+    <Aaa />
   </div>
 </template>
 
 <script>
 import HelloWorld from "./components/HelloWorld.vue";
+import Aaa from "./components/Aaa.vue";
 
 export default {
   name: "App",
   components: {
     HelloWorld,
+    Aaa,
   },
 };
 </script>
