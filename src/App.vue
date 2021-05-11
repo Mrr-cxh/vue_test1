4f60e68011c08b4b10d5d6ce628dbae6399acc17
Mrr-cxh <1361091680@qq.com>
2021-05-11 20:15:27 +0800

    bbb

--- src/App.vue
+++ src/App.vue
@@ -3,18 +3,21 @@
     <img alt="Vue logo" src="./assets/logo.png" />
     <HelloWorld msg="Welcome to Your Vue.js App" />
     <Aaa />
+    <Bbb />
   </div>
 </template>
 
 <script>
 import HelloWorld from "./components/HelloWorld.vue";
 import Aaa from "./components/Aaa.vue";
+import Bbb from "./components/Bbb.vue";
 
 export default {
   name: "App",
   components: {
     HelloWorld,
     Aaa,
+    Bbb,
   },
 };
 </script>
